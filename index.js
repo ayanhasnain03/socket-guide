a2372a38775065951516c1da94b9d51eafb100c2
Ayan <ayan2572006@gmail.com>
2024-10-01 00:49:49 +0530

    connection both side

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import http from "http";
-import { fileURLToPath } from "node:url";
-import { dirname, join } from "node:path";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 import { Server } from "socket.io";
 
 const app = express();
@@ -9,9 +9,16 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
-app.listen(3000, () => {
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  console.log(socket.id);
+});
+
+server.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
